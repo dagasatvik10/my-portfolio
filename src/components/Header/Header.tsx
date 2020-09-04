5881132e2544dfbf4e80ba6bded2fd40a95d5f54
dagasatvik10 <dagasatvik10@gmail.com>
2020-09-05 02:05:35 +0530

    test: add unit tests for header component
    - update header component to include aria roles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,15 +10,24 @@ export const HeaderComponent: FunctionComponent = () => {
   return (
     <>
       <div className="flex flex-row justify-between p-5 h-auto items-center border-b-2 border-solid border-gray-200">
-        <div className="font-black text-black text-xl md:text-3xl font-title cursor-pointer">SATVIK DAGA</div>
+        <div
+          role="heading"
+          aria-level={1}
+          className="font-black text-black text-xl md:text-3xl font-title cursor-pointer"
+        >
+          SATVIK DAGA
+        </div>
         <div>
           <div
+            role="button"
+            aria-pressed="false"
+            aria-label="menu"
             className="md:hidden cursor-pointer"
             onClick={() => {
               setMenuOpen(!menuOpen);
             }}
           >
-            {menuOpen ? <XIcon /> : <MenuIcon />}
+            {menuOpen ? <XIcon title="x" /> : <MenuIcon title="menu" />}
           </div>
         </div>
       </div>
