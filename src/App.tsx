d3b09b99b4f0b96e680818db7c8ef8691082b297
dagasatvik10 <dagasatvik10@gmail.com>
2020-09-10 00:47:59 +0530

    feat: add routing
    - add home component with lazy loading
    - add portfolio, contact routes with text children

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React, { FunctionComponent } from 'react';
+import { LoaderComponent } from 'components/Loader/Loader';
+import React, { FunctionComponent, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { HeaderComponent } from './components/Header';
 
+const HomeComponent = lazy(() => import('./domain/Home'));
+
 const App: FunctionComponent = () => {
   return (
-    <div className="container mx-auto bg-white flex flex-col font-body text-primary1 h-screen overflow-scroll">
-      <HeaderComponent />
-    </div>
+    <Router>
+      <div className="container mx-auto bg-white flex flex-col font-body h-screen overflow-scroll text-primary1">
+        <HeaderComponent />
+        <Suspense fallback={<LoaderComponent />}>
+          <Switch>
+            <Route exact path="/" component={HomeComponent} />
+            <Route path="/portfolio">Portfolio</Route>
+            <Route path="/contact">Contact Me</Route>
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
   );
 };
 
