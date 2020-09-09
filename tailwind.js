09c57a668d3509232f023a4b08f120ead6773050
dagasatvik10 <dagasatvik10@gmail.com>
2020-09-09 21:21:22 +0530

    fix: mobile menu slide in animation on open
    - add slide in y animation

--- tailwind.js
+++ tailwind.js
@@ -653,6 +653,7 @@ module.exports = {
       ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
       pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       bounce: 'bounce 1s infinite',
+      slideInY: 'slideInY 1s',
     },
     keyframes: {
       spin: {
@@ -672,6 +673,16 @@ module.exports = {
         '50%': {
           transform: 'none',
           animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+        },
+      },
+      slideInY: {
+        from: {
+          opacity: 0,
+          transform: 'translateY(-100%)',
+        },
+        to: {
+          opacity: 1,
+          transform: 'translateY(0)',
         },
       },
     },
