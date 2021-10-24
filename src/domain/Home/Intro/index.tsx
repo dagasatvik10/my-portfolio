6f75010c2e32c8abc1c857a4b35b6544d4253215
Satvik Daga <dagasatvik10@gmail.com>
2021-10-24 17:29:38 +0530

    fix: fix height for intro section

--- src/domain/Home/Intro/index.tsx
+++ src/domain/Home/Intro/index.tsx
@@ -8,7 +8,7 @@ import { differenceInYearsAndMonths } from './utilities';
 const Intro: FunctionComponent = () => {
   const { years: yearsOfExp, months: monthsOfExp } = differenceInYearsAndMonths(JOB_START_DATE, new Date());
   return (
-    <div className="relative w-full h-400 sm:h-600 flex flex-col items-start justify-between px-2">
+    <div className="relative w-full h-350 sm:h-500 flex flex-col items-start justify-between px-2">
       <div className="flex flex-col z-10 justify-between">
         <div className="font-heading text-4xl sm:text-6xl lg:text-8xl animate-bounce pt-5">Hello! I'm Satvik Daga</div>
         <div className="text-xl sm:text-2xl lg:text-4xl pb-5 pt-2">
