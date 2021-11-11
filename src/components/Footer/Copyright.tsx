829f9f7fe6557ec74fedbfe366a69f1c675fdfc4
Satvik Daga <dagasatvik10@gmail.com>
2021-11-12 00:31:00 +0530

    fix: update footer styling
    - change top margin for social media icons
    - change text color of dev.to link
    - update copyright year to current year

--- src/components/Footer/Copyright.tsx
+++ src/components/Footer/Copyright.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from 'react';
 
 export const CopyrightComponent: FunctionComponent = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row justify-center items-center">Copyright &#169; 2020</div>
+      <div className="flex flex-row justify-center items-center">Copyright &#169; {currentYear}</div>
       <div className="flex flex-row justify-center items-center">Satvik Daga</div>
     </div>
   );
